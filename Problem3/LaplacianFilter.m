55df3af1f495f2a023dd315d738eb26bce19b71e
TB-ME <thomascbentham@gmail.com>
2019-09-18 11:27:53 +1000

    Adding all last nights work, should remove folders and add them under one folder

---
+++ Problem3/LaplacianFilter.m
@@ -0,0 +1,14 @@
+function [Laplacian] = LaplacianFilter(I)
+%Laplacian Filter runs an Intensity image through a laplacian filter
+[m,n] = size(I);
+Laplacian = zeros(m,n);
+for i = 2:m-1
+    for j = 2:n-1 
+        Laplacian(i,j) = -1*I(i-1,j-1)-1*I(i-1,j)-1*I(i-1,j+1) ...
+                         -1*I(i,j-1)+8*I(i,j)-1*I(i,j+1) ...
+                         -1*I(i+1,j-1)-1*I(i+1,j)-1*I(i+1,j+1);
+       %Could do corner check here
+    end
+end
+end
+
---
+++ Problem3/problem3.m
@@ -0,0 +1,26 @@
+%Loading the images
+wd = imread('WhiteDiamond2019.tif');
+wd = wd(:,:,1:3);
+
+wr = imread('WhiteRectangle2019.tif');
+wr = wr(:,:,1:3);
+
+ws = imread('WhiteRectangle2019.tif');
+ws = ws(:,:,1:3);
+
+wt = imread('WhiteTriangle2019.tif');
+wt = wt(:,:,1:3);
+
+%Convert to intensity
+wdI = double(wd(:,:,1)/3 + wd(:,:,2)/3 + wd(:,:,3)/3);
+wrI = double(wr(:,:,1)/3 + wr(:,:,2)/3 + wr(:,:,3)/3);
+wsI = double(ws(:,:,1)/3 + ws(:,:,2)/3 + ws(:,:,3)/3);
+wtI = double(wt(:,:,1)/3 + wt(:,:,2)/3 + wt(:,:,3)/3);
+%Run through Laplacian Filter
+[m,n] = size(I);
+
+%Filtered Images
+WDF = LaplacianFilter(wdI);
+WRF = LaplacianFilter(wrI);
+WSF = LaplacianFilter(wsI);
+WTF = LaplacianFilter(wtI);
