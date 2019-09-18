4b0fe27f1b7e547bc7c84a9c847745589b726a5d
Connor Jarvis <s3545505@student.rmit.edu.au>
2019-09-18 11:48:05 +1000

    Testing commit

--- problem5.m
+++ problem5.m
@@ -1,11 +1,11 @@
 %Import image
 WhiteMix = imread('WhiteMix2019.tif');
 
-I = double(WhiteMix(:,:,1)/3+WhiteMix(:,:,2)/3+WhiteMix(:,:,3)/3);
+I = double(WhiteMix(:,:,1)/3 + WhiteMix(:,:,2)/3 + WhiteMix(:,:,3)/3);
 
 %Apply Filter
 
-%Follow Line 
+%Follow Line
 
 %Crop out shape
 
