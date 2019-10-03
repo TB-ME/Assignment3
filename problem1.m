240244e747a312a87c5a6edd99fff6ebb2c350cf
DESKTOP-NTN8MLH\Thomas <thomascbentham@gmail.com>
2019-10-03 16:12:52 +1000

    fixed and completed problem 1

--- problem1.m
+++ problem1.m
@@ -1,15 +1,15 @@
 % Read Image
-img = imread('AppleTree.png');
+img = imread('Resources/AppleTree.png');
 
-RED = img(:,:,1)
-GREEN = img(:,:,2)
-BLUE = img(:,:,3)
+RED = img(:,:,1);
+GREEN = img(:,:,2);
+BLUE = img(:,:,3);
 % transform image into intensities
 
 
 %Array to store the locations of the apples
-tempApples = zeros(10,2);
-numApples = 1;
+Apples = zeros(10,2);
+numApples = 0;
 
 len = size(img);
 
@@ -17,12 +17,19 @@ for i = 1:len(1)
     for j = 1:len(2)
         %If x is above a threshold then the color is red
         if (RED(i,j) >= 200) && GREEN(i,j) <= 100 && BLUE(i,j) <= 100
-            tempApples(numApples,1) = i;
-            tempApples(numApples,2) = j;
+            Apples(numApples+1,1) = i;
+            Apples(numApples+1,2) = j;
             numApples = numApples + 1;
         end
     end
 end
 
-Apples = tempApples((Apples(:,1) > 0),:);
+%Displaying the Number of Apples and Apple Locations
+disp("NUMBER OF APPLES: " + numApples);
+
+for i = 1:length(Apples)
+    if Apples(i,1) ~= 0
+        disp("APPLE LOCATION " + i + ": " + Apples(i,1) + "," + Apples(i,2));
+    end
+end
     
